9a0db85b272354dcad342b4d132c0149db9e8fa1
Lakshmireddy012 <Lakshmireddy012@gmail.com>
2020-01-26 19:21:57 +0530

    Ionic 4 angular 8 starter first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  {path: '', loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)},
+  {path: 'register', loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)},
+  {path: 'about', loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)},
+  {path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)},
+  {path: 'edit-profile', loadChildren: () => import('./pages/edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)},
+  {path: 'home-results', loadChildren: () => import('./pages/home-results/home-results.module').then( m => m.HomeResultsPageModule)},
+  {path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
+
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
